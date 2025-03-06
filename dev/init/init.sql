bcfad100761eb6d9ca3bdc0ae52270744e57de62
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2025-03-06 12:35:35 +0100

    add ontology tree route

--- dev/init/init.sql
+++ dev/init/init.sql
@@ -9,5 +9,30 @@ create table modules
     primary key (id)
 );
 
-copy modules(id,name,fdpg_cds_code,fdpg_cds_system,fdpg_cds_version,version)
-from '/docker-entrypoint-initdb.d/modules.csv' DELIMITER ','  CSV HEADER;
+copy modules (id, name, fdpg_cds_code, fdpg_cds_system, fdpg_cds_version, version)
+    from '/docker-entrypoint-initdb.d/modules.csv' DELIMITER ',' CSV HEADER;
+
+create table concepts
+(
+    id                       uuid    not null,
+    module_id                uuid    not null
+        constraint concepts_modules_id_fk
+            references modules,
+    parent_id                uuid
+        constraint concepts_concepts_id_fk
+            references concepts,
+    display                  text    not null,
+    term_codes               jsonb,
+    selectable               boolean not null,
+    leaf                     boolean not null,
+    time_restriction_allowed boolean,
+    filter_type              text,
+    filter_options           jsonb,
+    version                  text    not null,
+    primary key (id)
+);
+
+copy concepts (id, module_id, parent_id, display, term_codes, selectable, leaf, time_restriction_allowed, filter_type,
+               filter_options, version)
+    from '/docker-entrypoint-initdb.d/concepts.csv' DELIMITER ',' CSV HEADER;
+
--- src/concept.rs
+++ src/concept.rs
@@ -1,43 +1,61 @@
-use std::sync::Arc;
-use axum::{debug_handler, Json, Router};
-use axum::extract::{Path, State};
-use axum::routing::get;
-use serde_derive::{Deserialize, Serialize};
-use sqlx::FromRow;
 use crate::error::ApiError;
 use crate::server::ApiContext;
-use sqlx::types::Uuid;
+use axum::extract::{Path, State};
+use axum::routing::get;
+use axum::{debug_handler, Router};
+use serde_derive::{Deserialize, Serialize};
+use sqlx::types::{JsonValue, Uuid};
+use sqlx::FromRow;
+use std::sync::Arc;
+
+// #[derive(Deserialize, Serialize, FromRow)]
+// struct Coding {
+//     code: String,
+//     system: String,
+//     display: String,
+//     version: String,
+// }
 
 #[derive(Deserialize, Serialize, FromRow)]
 struct Concept {
     id: Uuid,
-    name: String,
+    display: String,
     parent_id: Option<Uuid>,
-    module_id: Option<Uuid>,
-    // ...
- }
+    module_id: Uuid,
+    term_codes: Option<JsonValue>,
+    leaf: bool,
+    time_restriction_allowed: Option<bool>,
+    filter_type: Option<String>,
+    selectable: bool,
+    //    filter_options: Option<String>,
+    filter_options: Option<JsonValue>,
+    // filter_options: Option<Json<Vec<Coding>>>,
+    version: String,
+}
 
 pub(crate) fn router() -> Router<Arc<ApiContext>> {
-    Router::new()
-        .route("/ontology/{id}", get(ontology))
+    Router::new().route("/ontology/{id}", get(ontology))
 }
 
 #[debug_handler]
 async fn ontology(
     State(ctx): State<Arc<ApiContext>>,
     Path(id): Path<Uuid>,
-) -> Result<Json<Vec<Concept>>, ApiError> {
-    let result = sqlx::query_as!(Concept,
-        // "select * from concepts where id = $1",
+) -> Result<axum::Json<Vec<Concept>>, ApiError> {
+    let result = sqlx::query_as!(
+        Concept,
         r#"WITH RECURSIVE ontology AS
-            ( select id,name,parent_id,module_id
+            ( select *
                from concepts where module_id = $1 and parent_id is null
                UNION ALL SELECT c.* FROM concepts c
                JOIN ontology on c.parent_id = ontology.id )
-           SELECT id as "id!", name as "name!",parent_id,module_id from ontology"#,
-        id)
-        .fetch_all(&ctx.db)
-        .await?;
+           SELECT id as "id!", display as "display!",parent_id,module_id as "module_id!",
+                  term_codes,leaf as "leaf!",time_restriction_allowed,filter_type,
+                  selectable as "selectable!",filter_options,version as "version!" from ontology"#,
+        id
+    )
+    .fetch_all(&ctx.db)
+    .await?;
 
-    Ok(Json(result))
+    Ok(axum::Json(result))
 }
